c5eac8a04731233467bff42c94bd2b99f6aa839d
imalzy <imalzaya19@gmail.com>
2021-02-22 22:42:28 +0700

    add package ejs

--- express_todo/app.js
+++ express_todo/app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index');
 });
 
 app.get('/add-item', (req, res) => {
-  res.sendFile('./views/add-item.html', { root: __dirname });
+  res.render('add-item');
 });
 
-app.use((req, res)=>{
-  res.sendFile('./views/404.html', { root: __dirname });
-})
+app.use((req, res) => {
+  res.render('404');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
