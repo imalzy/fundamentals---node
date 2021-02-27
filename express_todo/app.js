b8c074e87b55bd11d6f0ef5924a8e6ab1d9aa61d
imalzy <imalzaya19@gmail.com>
2021-02-28 00:23:39 +0700

    add bodyParser, Post Request

--- express_todo/app.js
+++ express_todo/app.js
@@ -1,50 +1,67 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Item = require('./models/items');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Item = require("./models/items");
 const app = express();
 const port = 3000;
-const mongodb = 'mongodb+srv://imalzy:admin@cluster0.uibh2.mongodb.net/todo_db?retryWrites=true&w=majority'
-mongoose.connect(mongodb, {
-  useNewUrlParser: true, useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 2000
-}).then(() => {
-  console.log('connection ready')
-  app.listen(port, () => {
-    console.log(`Application Running at http://localhost:${port}`);
-  });
+const mongodb =
+  "mongodb+srv://imalzy:admin@cluster0.uibh2.mongodb.net/todo_db?retryWrites=true&w=majority";
+mongoose
+  .connect(mongodb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 2000,
+  })
+  .then(() => {
+    console.log("connection ready");
+    app.listen(port, () => {
+      console.log(`Application Running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => handleError(error));
 
-}).catch(error => handleError(error));
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
-
-app.set('view engine', 'ejs');
-
-app.get('/create', (req, res)=>{
+app.get("/create", (req, res) => {
   const item = new Item({
-    name: 'Earphone Sony',
-    price: 3500000
+    name: "Earphone Sony",
+    price: 3500000,
   });
-  item.save().then(result=>res.send(result));
+  item.save().then((result) => res.send(result));
 });
 
-app.get('/itembyid', (req, res)=>{
-  Item.findById({_id: "603857534f31da42a8b86658"}).then(result=>res.send(result));
+app.get("/itembyid", (req, res) => {
+  Item.findById({ _id: "603857534f31da42a8b86658" }).then((result) =>
+    res.send(result)
+  );
 });
 
-app.get('/', (req, res) => {
-  res.redirect('/list');
+app.get("/", (req, res) => {
+  res.redirect("/list");
 });
 
-app.get('/list', (req, res)=>{
-  Item.find().then(result=>{
-    res.render('index', { items: result });
+app.get("/list", (req, res) => {
+  Item.find().then((result) => {
+    res.render("index", { items: result });
   });
 });
 
-app.get('/add-item', (req, res) => {
-  res.render('add-item');
+app.get("/add-item", (req, res) => {
+  res.render("add-item");
+});
+
+app.post("/add", (req, res) => {
+  const item = new Item(req.body);
+  item
+    .save()
+    .then(() => {
+      res.redirect("/list");
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use((req, res) => {
-  res.render('404');
+  res.render("404");
 });
-
